4d192b31118d60886816cba02db34b23e54f3e88
rajesh479 <guntreddirajesh55@gmail.com>
2022-03-04 12:32:48 +0530

    login reducer dummy data added

---
+++ src/redux/reducer/login/loginReducer.js
@@ -0,0 +1,22 @@
+
+
+
+const initialState = {
+    userData : [
+        {name : 'Rajesh',
+         email : 'rajesh@gmail.com',
+         password : 'rajesh',
+         id : 1
+         },
+         {name : 'nagesh',
+         email : 'nagesh@gmail.com',
+         password : 'nagesh',
+         id : 2
+         },
+         {name : 'santhosh',
+         email : 'santhosh@gmail.com',
+         password : 'rsanthoshajesh',
+         id : 3
+         }
+    ] 
+}
